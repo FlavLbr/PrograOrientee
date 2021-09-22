88c78c7872166ee58a9281423eded6b15befee86
Flavien <flebreto@etud.univ-angers.fr>
2021-09-22 14:54:17 +0200

    Premier essai

---
+++ program.f90
@@ -0,0 +1,34 @@
+recursive function C(n,p) result(f)
+    integer, intent (in) :: n
+    integer :: f
+    integer :: p
+
+    if(p==0 .or. n==p) then 
+        f= 1
+    else
+        f=C(n-1,p)+C(n-1,p-1)
+    endif
+end function C
+
+program binom
+integer :: n
+integer :: p 
+integer :: C
+character(8)    :: arg_n
+character(8)    :: arg_p
+
+call getarg(1,arg_n)
+call getarg(2,arg_p)
+read(arg_n,*) n
+read(arg_p,*) p
+if (p<=n .and. n>=0 .and. p>=0) then
+    print*, "C(",n,",",p,")=",C(n,p)
+endif
+end program binom
+
+
+
+
+
+
+
